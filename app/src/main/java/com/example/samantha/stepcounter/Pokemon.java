bd0099c24bb6f908197e6778279e7c4076dd25ee
supergardie <sferreira12@myseneca.ca>
2018-03-18 02:42:00 -0400

    Add test moves to pokemon constructor, as well as notes/thoughts on how to actually handle moves

--- app/src/main/java/com/example/samantha/stepcounter/Pokemon.java
+++ app/src/main/java/com/example/samantha/stepcounter/Pokemon.java
@@ -35,7 +35,18 @@ public class Pokemon {
 
         this.level = 2;
         this.exp = 0;
+
+        // So I need to differentiate this by type... I guess I need a moves class?
+        // How should I set that up? I don't need to be creating new moves. I need a set number of moves already created from which to choose,
+        // based on having a matching/compatible type with this pokemon.
+        // Should I have a moves class, with predefined types, names, pp, and power?
+        // Should I create these move objects immediately upon loading the game?
         this.moves = new String[4];
+
+        this.moves[0] = "move 1";
+        this.moves[1] = "move 2";
+        this.moves[2] = "move 3";
+        this.moves[3] = "move 4";
     }
 
 }
