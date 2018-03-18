eff3c9efb9084981f73be5f6fc3bb5414419ad76
supergardie <sferreira12@myseneca.ca>
2018-03-18 02:53:24 -0400

    Add types class to hold things like pokemon types and trainer type prefixes

---
+++ app/src/main/java/com/example/samantha/stepcounter/Moves.java
@@ -0,0 +1,8 @@
+package com.example.samantha.stepcounter;
+
+/**
+ * Created by samantha on 18/03/18.
+ */
+
+public class Moves {
+}
---
+++ app/src/main/java/com/example/samantha/stepcounter/Types.java
@@ -0,0 +1,24 @@
+package com.example.samantha.stepcounter;
+
+/**
+ * Created by samantha on 18/03/18.
+ */
+
+public final class Types {
+    public final static String[] trainers = {
+            "Fire Trainer",
+            "Water Trainer",
+            "Grass Trainer",
+            "Youngster"
+    };
+
+    public final static String[] pokemon = {
+            "fire",
+            "water",
+            "grass"
+    };
+
+    private Types(){
+        // static!
+    }
+}
