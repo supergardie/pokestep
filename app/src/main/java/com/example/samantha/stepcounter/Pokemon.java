b936b65197240ee6a9ef2238f37fa73569005aad
supergardie <sferreira12@myseneca.ca>
2018-03-18 02:33:53 -0400

    Start on pokemon class

--- app/src/main/java/com/example/samantha/stepcounter/Pokemon.java
+++ app/src/main/java/com/example/samantha/stepcounter/Pokemon.java
@@ -1,19 +1,41 @@
 package com.example.samantha.stepcounter;
 
+import java.util.Random;
+
 /**
  * Created by samantha on 18/03/18.
  */
 
 public class Pokemon {
     private static final String[] types = {
-            "Water",
-            "Grass",
-            "Fire"
+        "Water",
+        "Grass",
+        "Fire"
+    };
+
+    private static final String[] pokemon = {
+        "Charmander",
+        "Bulbasaur",
+        "Squirtle"
     };
 
     public String name;
     public String type;
     public int level;
+    public String[] moves;
+    public int exp;
 
+    public Pokemon() {
+        Random rand = new Random();
+        int randNum = rand.nextInt(pokemon.length - 1) + 1;
+        this.name = pokemon[randNum];
+
+        randNum = rand.nextInt(types.length - 1) + 1;
+        this.type = types[randNum];
+
+        this.level = 2;
+        this.exp = 0;
+        this.moves = new String[4];
+    }
 
 }
