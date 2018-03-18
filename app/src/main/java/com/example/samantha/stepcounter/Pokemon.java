1fb184e7e98a7b1027e5847bc1c24a6b497e51ce
supergardie <sferreira12@myseneca.ca>
2018-03-18 19:30:02 -0400

    Nah, maybe pokemon should be an abstract class...

--- app/src/main/java/com/example/samantha/stepcounter/Pokemon.java
+++ app/src/main/java/com/example/samantha/stepcounter/Pokemon.java
@@ -4,24 +4,36 @@ package com.example.samantha.stepcounter;
  * Created by samantha on 18/03/18.
  */
 
-public interface Pokemon {
-    public String name = "";
-    public String type = "";
+public abstract class Pokemon {
+    public String name;
+    public String type;
 
-    public int level = 0;
-    public int exp = 0;
+    public int level;
+    public int exp;
 
-    public Moves[] moves = {};
+    public Moves[] moves;
 
-    public int hp = 0;
-    public int att = 0;
-    public int special = 0;
-    public int def = 0;
+    public int hp;
+    public int att;
+    public int special;
+    public int def;
 
-        // So I need to differentiate this by type... I guess I need a moves class?
-        // How should I set that up? I don't need to be creating new moves. I need a set number of moves already created from which to choose,
-        // based on having a matching/compatible type with this pokemon.
-        // Should I have a moves class, with predefined types, names, pp, and power?
-        // Should I create these move objects immediately upon loading the game?
+    // So I need to differentiate this by type... I guess I need a moves class?
+    // How should I set that up? I don't need to be creating new moves. I need a set number of moves already created from which to choose,
+    // based on having a matching/compatible type with this pokemon.
+    // Should I have a moves class, with predefined types, names, pp, and power?
+    // Should I create these move objects immediately upon loading the game?
 
-    public void levelUp();
+    public void levelUp() {
+        this.level++;
+        // other stuff
+    }
+
+    public void fainted() {
+        // something
+    }
+
+    public void endBattle() {
+        // exp up... anything else?
+    }
+}
